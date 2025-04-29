29c647c238fb2d2b5b878e440c52198da0578f80
choesourcetree <david.h.choe@gmail.com>
2025-04-29 15:49:04 -0500

    Added this project to Firebase

--- app/src/main/java/com/example/mymttqdemo2/MainActivity.kt
+++ app/src/main/java/com/example/mymttqdemo2/MainActivity.kt
@@ -10,7 +10,6 @@ import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
-
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
